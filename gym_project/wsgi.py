d0adc74d1080f53e1063b6e21b8d00acf49cdda7
Tsafack Nestor <nestor.tsafack@yahoo.fr>
2025-06-26 09:06:11 -0400

    Ajout de la création auto du superadmin sur Render

--- gym_project/wsgi.py
+++ gym_project/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for gym_project project.
+# gym_project/wsgi.py
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_project.settings')
 
 application = get_wsgi_application()
+
+# Création automatique du superutilisateur sur Render
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="nestor.tsafack@yahoo.fr").exists():
+    User.objects.create_superuser(
+        username="nestor.tsafack@yahoo.fr",
+        email="nestor.tsafack@yahoo.fr",
+        password="Admin1234"
+    )
