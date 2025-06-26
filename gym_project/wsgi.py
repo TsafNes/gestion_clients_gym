732482c9e0fd59e5e3d2b04dd3bac497c8c42e77
Tsafack Nestor <nestor.tsafack@yahoo.fr>
2025-06-26 10:43:32 -0400

    🧹 Nettoyage du seed auto après initialisation

--- gym_project/wsgi.py
+++ gym_project/wsgi.py
@@ -6,10 +6,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_project.settings')
 
 application = get_wsgi_application()
-
-# Ajout : exécuter la commande seed_data une seule fois
-try:
-    from django.core.management import call_command
-    call_command('seed_data')
-except Exception as e:
-    print(f"Erreur lors du seed : {e}")
